05c61be4b8cd8e7d424e51766f4f70134481a1ae
Konstantin Vozniuk <kvoznjuk@gmail.com>
2021-10-04 22:17:13 +0300

    Part 5a

--- src/main/java/net/voznjuk/fp/controller/MainController.java
+++ src/main/java/net/voznjuk/fp/controller/MainController.java
@@ -6,12 +6,10 @@ import net.voznjuk.fp.repos.InvoiceRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
